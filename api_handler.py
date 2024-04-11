d54ad2bead6ebd181b3cd9b3ab3c16554519c533
Jshihillyard <146875797+Jshihillyard@users.noreply.github.com>
2024-04-10 22:31:27 -0700

    Update api_handler.py

--- api_handler.py
+++ api_handler.py
@@ -12,19 +12,5 @@ def get_submission_query(query):
     submission_data = submission_data.json()["data"]  
     return submission_data
 
-#hugging face transformers
-    #roberta
-    #distilbert
-    #building a frontend
-#AAAAAAAAAAAAAAAAAAAAAPiUtAEAAAAAbGuhP%2Ftucc1z6ZdAzvRQCe6zdeE%3Dib4rHbAneRHjXsSC42h8dHVnacrIVc1Qo7rOUS1LI3e0wmbTaM
-#Bearer token
-#1211550517727395840-ZuAAUoi4F49sUNHpMf9yasjOvBwTk5
-#access token
-#rU9AfAEzLNG2ORTfZkSi7sqMhLTuHU0gAIpSB7GEwBVn0
-#access token secret
-#XYhCPfFvmVqMg4N5MWFr2zzOb
-#API Key
-#xCPnRu2AuKaI24g3ca7w0Ir7hWMB24LDOMhdV7gR4poDMW6Gdl
-#API Key secret 
 
 
